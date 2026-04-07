2ef5616ccec7f77351ba9eb762c5ba5e83796c98
Mehadi <mehadiraj09@gmail.com>
2026-04-07 14:44:29 +0600

    Update passgenerator.py

--- passgenerator.py
+++ passgenerator.py
@@ -1,16 +1,17 @@
 names = input("Enter the names:").split()
 years = input("Enter the years:").split()
 symbols = input("Enter the symbols:").split()
-
 passlenthuL = int(input("Enter the password lower length:")) 
 passlenthuU = int(input("Enter the password lower length:"))
-
+file_name = input("Enter the file name: ")
 for name in names:
     for year in years:
         for sym in symbols:
-            
-            passowrds = name + sym + year + sym
-          
-            x = len(passowrds)
-            if x>=passlenthuL and x<=passlenthuU:
-                print(passowrds)
+            with open(file_name, "a") as f:
+                passowrds = name + sym + year + sym 
+             
+
+                x = len(passowrds)
+                if x>=passlenthuL and x<=passlenthuU:
+                    f.write(passowrds + "\n")
+                    
